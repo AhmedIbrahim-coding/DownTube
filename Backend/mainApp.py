29b153f6b71670299fc7c60529d8f56380d45b48
Ahmed_Eladawy <ahmed.ibrahim.eladawy7@gmail.com>
2026-02-15 09:52:41 +0200

    Add download initial action & create a temporary past button

--- Backend/mainApp.py
+++ Backend/mainApp.py
@@ -3,3 +3,7 @@
 class BackApp():
     def __init__(self):
         pass
+
+
+    def check_existance(self):
+        print("start looking for the video")
--- Frontend/main_UI.py
+++ Frontend/main_UI.py
@@ -3,12 +3,17 @@ import customtkinter as ctk
 import tkinter as tk
 from tkinter import filedialog, font as tkFont
 
+# local imports
+from Backend.mainApp import BackApp
+
 class FrontApp(ctk.CTk):
     def __init__(self, version):
         super().__init__()
         self.create_main_window(version)
         self.create_entry_field()
         self.create_download_button()
+
+        self.backApp = BackApp()
 
 
     def create_main_window(self, version):
@@ -34,9 +39,12 @@ class FrontApp(ctk.CTk):
         
         self.linkEntry.pack(pady=20)
 
-    def creat_past_button(self):
+        self.create_past_button()
+
+    def create_past_button(self):
         #create a button on top of the entry field that get the copied link
-        pass
+        past_button = ctk.CTkButton(master=self.linkEntry, width=40, height=20, text="Past")
+        past_button.place(relx=1, rely=0.5, anchor="center", x=-25)
 
     def create_download_button(self):
         # start download button
@@ -45,6 +53,17 @@ class FrontApp(ctk.CTk):
                                              width=80,
                                              height=30,
                                              corner_radius=3,
-                                             font=("Arial", 14))
+                                             font=("Arial", 14),
+                                             command=self.on_download_click)
         
         self.download_button.place(relx=1, rely=1, anchor="center", x=-70, y=-30)
+
+    def on_download_click(self):
+        try:
+            link = self.linkEntry.get()
+            if not link:
+                raise Exception("Please enter a link first.")
+
+            self.backApp.check_existance()
+        except Exception as e:
+            print(e)
